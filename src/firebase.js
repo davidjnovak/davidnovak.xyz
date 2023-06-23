ae72159488c264f263db841cbacdb61d55c96551
davidjnovak <51279385+davidjnovak@users.noreply.github.com>
2023-06-23 14:58:23 -0400

    templation

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNYESukWfIZix05bP1HAfNt45vp6XWspU",
+  authDomain: "portfolio-4dcb9.firebaseapp.com",
+  projectId: "portfolio-4dcb9",
+  storageBucket: "portfolio-4dcb9.appspot.com",
+  messagingSenderId: "894686413307",
+  appId: "1:894686413307:web:e6d5a40962be2dbf760b5e",
+  measurementId: "G-HKGSZP6NZQ"
+};
+
+const app = initializeApp(firebaseConfig);
